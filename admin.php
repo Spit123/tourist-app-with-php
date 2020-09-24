bb2cbb1f923efecf544bfa039f3bde0d034697f9
Spit123 <51290114+Spit123@users.noreply.github.com>
2020-09-24 19:24:24 +0300

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,17 @@
+<? include("php/login.php");
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+             <title>Админка</title>
+             <link rel="stylesheet" href="admin.css">
+</head>
+<body>
+             <div class='wrapper'>
+                           <main class='main' id='main'>
+                                          <?echo $echo;?>
+                          </main>
+             </div>
+</body>
+</html>
